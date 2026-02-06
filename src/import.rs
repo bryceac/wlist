982ab01db3117e6ed2d78e4fab39527d84b0075b
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-06 14:05:20 -0800

    add module for import functionality

---
+++ src/import.rs
@@ -0,0 +1,30 @@
+use clap::Parser;
+use wlitem::Save;
+
+use crate::{shared::real_path, database::load_items_from_db};
+
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>", long_about = "export wishlist.")]
+pub struct Export {
+    #[clap(default_value = "~/wishlist/gift_registry.db")]
+    pub file_path: String,
+
+    #[clap(long, short)]
+    pub output_file: String
+}
+
+impl Export {
+    pub fn run(&self) {
+        let destination_path = real_path(&self.output_file);
+        let items = load_items_from_db(&self.file_path);
+
+        match destination_path {
+            ref p if p.ends_with(".json") => if let Err(error) = items.save(p) {
+                println!("{}", error);
+            },
+            _ => if let Err(error) = items.save_tsv(&destination_path) {
+                println!("{}", error)
+            }
+        }
+    }
+}
