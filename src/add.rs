89c8ce36ef4700e4bb2e6036082d8f3c087b9c21
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-05 17:28:59 -0800

    keep working on add command.

--- src/add.rs
+++ src/add.rs
@@ -1,5 +1,24 @@
 use clap::Parser;
+use wlitem::Priority;
 
 #[derive(Parser)]
 #[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
-pub struct Add;
+pub struct Add {
+    #[clap(default_value = "~/wishlist/gift_registry.db")]
+    pub file_path: String,
+
+    #[clap(long, short)]
+    pub name: String,
+
+    #[clap(long, short)]
+    pub quantity: u32,
+
+    #[clap(long, short, default_value = "medium")]
+    pub priority: Priority,
+
+    #[clap(long, short, default_value = "")]
+    pub url: String,
+
+    #[clap(long, num_args = 0.., value_delimiter = ',', required = false)]
+    pub notes: Vec<String>
+}
