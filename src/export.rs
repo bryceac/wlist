c461b353993aa2266cd2d12c8ff403c3f96fc386
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-06 13:53:45 -0800

    start adding in logic to run function for export.

--- src/export.rs
+++ src/export.rs
@@ -1,6 +1,8 @@
 use clap::Parser;
 use wlitem::{ Item, Save };
 
+use crate::{shared::real_path, database::load_items_from_db};
+
 #[derive(Parser)]
 #[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>", long_about = "export wishlist.")]
 pub struct Export {
@@ -9,4 +11,16 @@ pub struct Export {
 
     #[clap(long, short)]
     pub output_file: String
+}
+
+impl Export {
+    pub fn run(&self) {
+        let destination_path = real_path(&self.output_file);
+        let items = load_items_from_db(&self.file_path);
+
+        match destination_path {
+            p if p.ends_with(".json") => {},
+            _ => {}
+        }
+    }
 }
