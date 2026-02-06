fc233ab2eba6190a397021e04116f687aa60fb10
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-06 13:48:18 -0800

    implement the basic structure of exporting.

--- src/export.rs
+++ src/export.rs
@@ -1,2 +1,12 @@
 use clap::Parser;
 use wlitem::{ Item, Save };
+
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>", long_about = "export wishlist.")]
+pub struct Export {
+    #[clap(default_value = "~/wishlist/gift_registry.db")]
+    pub file_path: String,
+
+    #[clap(long, short)]
+    pub output_file: String
+}
