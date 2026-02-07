e15954136c090e83111ae6fdd3067d9c2230d32b
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-06 20:37:38 -0800

    create basic structure of update command.

---
+++ src/update.rs
@@ -0,0 +1,14 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>", long_about = "update items and notes.")]
+pub struct Update {
+    #[clap(default_value = "~/wishlist/gift_registry.db")]
+    pub file_path: String,
+
+    #[clap(long, short)]
+    pub item_id: Option<String>,
+
+    #[clap(long, short)]
+    pub note_id: Option<u32>
+}
