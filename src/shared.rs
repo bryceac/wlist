713bf0e4cec31e9b09eda2a3b1d034af8a9e25c0
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-09 10:00:47 -0800

    add way to read file contents.

--- src/shared.rs
+++ src/shared.rs
@@ -1,3 +1,5 @@
+use std::{ fs::File, io::{ self, Read }};
+
 use url_path::UrlPath;
 
 pub fn real_path(p: &str) -> String {
@@ -6,4 +8,12 @@ pub fn real_path(p: &str) -> String {
     } else {
         UrlPath::new(p).normalize()
     }
+}
+
+pub fn file_content(p: &str) -> Result<String, io::Error> {
+    let mut file_content = String::new();
+
+    File::open(p)?.read_to_string(&mut file_content)?;
+
+    Ok(file_content)
 }
