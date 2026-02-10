180e6eae93f56768efdc956c64977fe5de6a11ef
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-09 21:43:46 -0800

    keep working on formating stuff.

--- src/export.rs
+++ src/export.rs
@@ -49,7 +49,7 @@ fn generate_html(items: Vec<Item>, title: &str) -> String {
     html.push_str("\t\t\t</header>");
     html.push_str("\t\t\t<hr>");
     html.push_str("\t\t\t<footer>");
-    html.push_str("\t\t\t<hr>");
+    html.push_str("\t\t\t\t<hr>");
     html.push_str("\t\t\t</footer>");
     html.push_str("\t\t</article>");
     html.push_str("\t</body>\r\n");
