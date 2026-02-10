787984c0ed70d770af90edc56137ffa4e75b2e99
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-10 14:44:03 -0800

    move horizontal rule to header.

--- src/export.rs
+++ src/export.rs
@@ -53,8 +53,8 @@ fn generate_html(items: &Vec<Item>, title: &str) -> String {
     html.push_str("\t\t<article>");
     html.push_str("\t\t\t<header>");
     html.push_str(&format!("\t\t\t\t<h1>{}</h1>", title));
+    html.push_str("\t\t\t\t<hr>");
     html.push_str("\t\t\t</header>");
-    html.push_str("\t\t\t<hr>");
     html.push_str(&registry(items));
     html.push_str("\t\t\t<footer>");
     html.push_str(&format!("\t\t\t\t<h2>Notes</h2>"));
