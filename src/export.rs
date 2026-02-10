b5c45ee75e834f51102054f5930d838924aabc13
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-09 21:30:09 -0800

    keep working on basic structure of html.

--- src/export.rs
+++ src/export.rs
@@ -43,6 +43,8 @@ fn generate_html(items: Vec<Item>, title: &str) -> String {
     html.push_str(&format!("\t\t<title>{}</title>", title));
     html.push_str("\t</head>\r\n");
     html.push_str("\t<body>\r\n");
+    html.push_str("\t\t<article>");
+    html.push_str("\t\t</article>");
     html.push_str("\t</body>\r\n");
     html.push_str("</html>");
 
