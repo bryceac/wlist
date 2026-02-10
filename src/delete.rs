6d5a78c0575ba207dc6a4f398c9461015adff2a6
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-10 14:36:32 -0800

    add help details to the delete command.

--- src/delete.rs
+++ src/delete.rs
@@ -3,9 +3,9 @@ use clap::Parser;
 use crate::database::{delete_item, item_with_id, delete_note_with_id};
 
 #[derive(Parser)]
-#[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>", about = "delete items or notes.")]
+#[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>", about = "delete items or notes.", long_about = "Delete items or notes from the wishlist.\r\n\r\nNotes and items can be deleted by passing the respective id, which can be discoered through the show command.\r\n\r\nHowever, you can only delete one or the other at a time.")]
 pub struct Delete {
-    #[clap(default_value = "~/wishlist/gift_registry.db")]
+    #[clap(default_value = "~/wishlist/gift_registry.db", help = "the path to the wishlist database")]
     pub file_path: String,
 
     #[clap(long, short)]
