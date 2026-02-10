88f97392303f6b174e0ca6a1cfd396224828fa41
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-09 21:56:38 -0800

    add note list to html.

--- src/export.rs
+++ src/export.rs
@@ -51,6 +51,7 @@ fn generate_html(items: Vec<Item>, title: &str) -> String {
     html.push_str("\t\t\t<footer>");
     html.push_str(&format!("\t\t\t\t<h2>Notes</h2>"));
     html.push_str("\t\t\t\t<hr>");
+    note_list(&items);
     html.push_str("\t\t\t</footer>");
     html.push_str("\t\t</article>");
     html.push_str("\t</body>\r\n");
