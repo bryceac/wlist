805d1c48934f562ed3f90e912785afef3fbf43d2
Bryce Campbell <tonyhawk2100@gmail.com>
2026-02-11 11:51:19 -0800

    update versions in necessary files.

--- src/wlist.rs
+++ src/wlist.rs
@@ -3,7 +3,7 @@ use clap::Parser;
 use crate::subcommand::SubCommand;
 
 #[derive(Parser)]
-#[clap(version = "0.1.0", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
+#[clap(version = "0.1.1", author = "Bryce Campbell <tonyhawk2100@gmail.com>")]
 pub struct WList {
     #[clap(subcommand)]
     pub subcommand: SubCommand
